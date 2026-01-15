fc588e8110bc523c5e5bd710631d656ff5af0bf9
Alchemmist <anton.ingrish@gmail.com>
2026-01-15 13:27:47 +0300

    fix problem with wg vpn

--- scripts/wireguard-manager.sh
+++ scripts/wireguard-manager.sh
@@ -4,30 +4,28 @@ SERVICE_NAME="wg-quick@wg0"
 STATUS_CONNECTED_STR='{"text":"Connected","class":"connected","alt":"connected", "tooltip": "VPN: cnnected"}'
 STATUS_DISCONNECTED_STR='{"text":"Disconnected","class":"disconnected","alt":"disconnected", "tooltip": "VPN: disconnected"}'
 
-# function askpass() {
-#   rofi -dmenu -password -no-fixed-num-lines -p "Sudo password : " -theme ~/scripts//rofi.rasi 
-# }
-
 function status_wireguard() {
-    sudo wg show wg0 &> /dev/null
+    sudo wg show wg0 &>/dev/null
     return $?
 }
 
+
 function toggle_wireguard() {
-  sudo resolvconf -u &&
-  status_wireguard && \
-     sudo wg-quick down wg0 || \
-     sudo wg-quick up wg0
+    if status_wireguard; then
+        sudo wg-quick down wg0
+        sudo systemctl restart NetworkManager
+    else
+        sudo resolvconf -u
+        sudo wg-quick up wg0
+    fi
 }
 
 case $1 in
-  -s | --status)
+-s | --status)
     status_wireguard && echo $STATUS_CONNECTED_STR || echo $STATUS_DISCONNECTED_STR
     ;;
-  -t | --toggle)
+-t | --toggle)
     toggle_wireguard
     ;;
-  *)
-    askpass
-    ;;
+*) ;;
 esac
